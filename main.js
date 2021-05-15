ac0fdb238c5dd2fd4ad6f0c206a5365fdf7dd9e7
bitakshat <akshatpal1232@gmail.com>
2021-05-15 22:24:42 +0530

    [+] new UI

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
 
 
 
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://coronavirus-smartable.p.rapidapi.com/stats/v1/US/",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-key": "913211ba91msh70c390896318456p150682jsne61ce7f5ff8e",
+        "x-rapidapi-host": "coronavirus-smartable.p.rapidapi.com"
+    }
+};
 
-
-const toggleButton = document.getElementsByClassName('toggle-button')[0];
-const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-const quoteSize = document.getElementById('heading');
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active');
+$.ajax(settings).done(function (response) {
+    console.log(response);
 });
-
-// chagnge font size
-const doResize = () => {
-    let width = window.innerWidth;
-    quoteSize.style.fontSize = `calc(${width} / 20)`;
-}
-doResize();
